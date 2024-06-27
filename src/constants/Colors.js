799a6306a4b8a272a86bff907f144b6fca7180f0
kuldeeepkumar <pro.kuldeepkumar@gmail.com>
2024-06-27 16:51:46 +0530

    Activity Indicator component added and paper packages installed , some folder sturcture correction

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,3 +1,4 @@
-export const COLORS = { 
-    INDICATOR_COLOR: "BLACK"
-}
+export const COLORS = {
+  INDICATOR_COLOR: "#6060A0",
+  LABEL_COLOR: "#f54293",
+};
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,47 +1,58 @@
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 
 import CustomButton from "../components/CustomButton";
-import React from "react";
+import Loader from "../components/Loader";
 import { containerStyle } from "../styles";
 
-
-const WelcomeImage = require("../../img/welcome.jpg");
+const WelcomeImage = require("../../assets/img/welcome.jpg");
 
 const Welcome = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <ImageBackground
-      imageStyle={{ opacity: 0.7 }}
-      source={WelcomeImage}
-      style={[containerStyle.container, {}]}>
-      <Text
-        style={{
-          fontSize: 100,
-          fontWeight: "bold",
-          color: "black",
-          fontFamily: "Inter-Black",
-          backgroundColor: "yellow",
-          paddingHorizontal: 10,
-        }}>
-        FITNESS
-      </Text>
-      <Text
-        style={{
-          fontSize: 45,
-          fontWeight: "bold",
-          color: "black",
-          fontFamily: "Inter-Black",
-          borderColor: "yellow",
-          borderWidth: 2,
-          padding: 12,
-        }}>
-         TRACKER APP
-      </Text>
+    <>
+      {isLoading === false ? (
+        <ImageBackground
+          source={WelcomeImage}
+          style={[containerStyle.container, {}]}>
+          <View
+            style={{
+              flex: 4,
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+            <Text
+              style={{
+                fontSize: 40,
+                fontWeight: "bold",
+                color: "black",
+                backgroundColor: "yellow",
+                paddingHorizontal: 10,
+              }}>
+              CURRENCY
+            </Text>
+            <Text
+              style={{
+                fontSize: 25,
+                fontWeight: "bold",
+                color: "#fff",
+                borderColor: "yellow",
+                borderWidth: 2,
+                padding: 12,
+                width: 233,
+              }}>
+              CONVERTER
+            </Text>
+          </View>
 
-      <View style={{ marginTop: 200 }}>
-        <CustomButton title={"Start New Journey"}></CustomButton>
-      </View>
-    </ImageBackground>
-
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            <CustomButton title={"Get Started"}></CustomButton>
+          </View>
+        </ImageBackground>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
