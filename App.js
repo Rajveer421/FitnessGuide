9548645be59d3bd74d7a7fddb9ffb27e4158c272
Rajveer421 <rajveersingh57594@gmail.com>
2024-06-29 13:34:04 +0530

    BottomTabNavigation screen

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./src/navigation/stackNavigation";
 import { StatusBar } from "expo-status-bar";
 import Welcome from "./src/pages/Welcome";
+import { BottomNavigation } from "react-native-paper";
+import BottomTabNavigation from "./src/navigation/bottomNavigation";
 
 export default function App() {
   return (
     <>
-      <Welcome />
+      {/*<Welcome />*/}
+      {/*<Navigation />*/}
+      <BottomTabNavigation />
     </>
   );
 }
